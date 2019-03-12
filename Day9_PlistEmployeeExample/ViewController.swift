cdbbdc10104cdebc3575d201cb1ad50ef04dc457
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-12 19:50:30 -0400

    Reading plist code added

--- Day9_PlistEmployeeExample/ViewController.swift
+++ Day9_PlistEmployeeExample/ViewController.swift
@@ -20,7 +20,28 @@ class ViewController: UIViewController {
         if let bundlePath = Bundle.main.path(forResource: "Employee", ofType: "plist") {
             let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
             
-            print(dictionary!.description)
+            //print(dictionary!.description)
+            
+            print("----- Country List From PList File -----")
+            let countryList = dictionary!["countries"] as! NSArray
+            print(countryList[0])
+            for c in countryList
+            {
+                print(c)
+            }
+            
+            print("----- Employee List From PList File -----")
+            
+            let employeeList = dictionary!["employees"] as! NSArray
+            
+            for e in employeeList
+            {
+                let emp = e as! NSDictionary
+                print("Employee ID      : \(emp["eid"]!)")
+                print("Employee Name    : \(emp["enm"]!)")
+                print("Employee Salary  : \(emp["salary"]!)")
+                print("----------------------------------")
+            }
         }
     }
 
